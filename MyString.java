5dd5041e15b8070bf088da7c25fbc524cb0e1272
isulley77 <isulley@stuy.edu>
2018-11-25 15:18:54 -0500

    charAt Method

--- MyString.java
+++ MyString.java
@@ -2,15 +2,25 @@ public class MyString implements CharSequence,Comparable<CharSequence>{
   
     private char[] data;
   
-    public MyString(CharSequence s){
+  
+    //Constructor
+    public MyString(CharSequence s){ 
     
+        //initalizes char array
         data = new char[s.length()];
-    
+        
+        //adds chars from CharSequence to array
         for(int i = 0, i < s.length(), i++){
             data[i] = s.charAt(i);
         }
     }
 
+    public char charAt(int index){
+        
+        //returns char
+        return data[i];
+    }
+    
     
     
 
